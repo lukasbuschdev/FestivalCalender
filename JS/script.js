84b62cc9087404e901212e1d56091019ca0a9848
lukasbuschdev <lukas.busch1996@gmx.at>
2024-03-27 09:16:34 +0100

    hopefully fixed the merge error from 26.03.2024

--- JS/script.js
+++ JS/script.js
@@ -14,8 +14,8 @@ async function init() {
 // GET DATASET
 // #######################################################################
 
-const dataset = [
-    "../database.json"
+const datasets = [
+    "../FestivalKalenderData.xlsx.csv"
 ];
 
 
@@ -25,6 +25,40 @@ const dataset = [
 // #######################################################################
 
 async function getData() {
-    const data = await (await fetch(dataset)).json();
-    return data;
+    const promises = datasets.map(dataset => fetchAndParseCSV(dataset));
+    const results = await Promise.all(promises);
+    const data = results.flat();
+    const updatedData = data.map((festival, index) => {
+        return {
+            ...festival,
+            id: index + 1
+        };
+    });
+    return updatedData;
+}
+
+async function fetchAndParseCSV(url) {
+    try {
+        const response = await fetch(url);
+        const text = await response.text();
+        const jsonData = CSVtoJSON(text);
+
+        return jsonData;
+    } catch (error) {
+        error("Error fetching or parsing the file:", error);
+    }
+}
+
+function CSVtoJSON(csv, delimiter = ',') {
+    const lines = csv.split('\n');
+    const headers = lines[0].split(delimiter);
+
+    return lines.slice(1).map(line => {
+        const values = line.split(delimiter);
+        return headers.reduce((object, header, index) => {
+            header = header.trim();
+            object[header] = values[index].trim();
+            return object;
+        }, {});
+    }).filter(obj => Object.keys(obj).length > 0);
 }
